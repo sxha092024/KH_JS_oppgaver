d8d561c6499ff0ba7c6f9890268ae0a42e52ac65
krola1 <kwl082024@gmail.com>
2024-11-18 13:45:20 +0100

    First

---
+++ main.js
@@ -0,0 +1,68 @@
+const myArray = ["apple", "banana", "cherry", "date", "eggplant"];
+
+//1.--------------------------------------------------------------------------------------
+// Print the numbers 1-10 using a "for-loop"
+
+//2.--------------------------------------------------------------------------------------
+// Print the numbers 1-10 using a "while loop"
+
+//3.--------------------------------------------------------------------------------------
+// Print each item in myArray as a string using a "for loop".
+
+//4.--------------------------------------------------------------------------------------
+// Using the myArray.push() method, add "mango" to the end of myArray.
+//print myArray using a for... in loop.
+
+//5. Using the myArray.splice() method, remove "cherry" from myArray
+// print the result using myArray.foreach()
+
+//6.--------------------------------------------------------------------------------------
+// Using the .pop() method, remove the last item from myArray.
+//print the result
+
+// 7.--------------------------------------------------------------------------------------
+// create a variable and assign it the content of myArray as a string using .join().
+// print your variable.
+//BONUS - See if you can make the items be seperated by a space
+
+//8--------------------------------------------------------------------------------------
+//Create a function that takes a number as an argument and returns that number squared.
+// call your function by using a valid argument
+function numberSquared(number) {}
+
+numberSquared();
+
+//9.--------------------------------------------------------------------------------------
+//Using the "%" (modulo) operator complete the function,
+// it should take "number" as an argument and return "even" if its even and "odd" if its odd.
+function oddOrEven(number) {}
+//call your function by using one of the options below.
+// oddOrEven(prompt());
+// oddOrEven()
+
+//10.--------------------------------------------------------------------------------------
+//write a function that prints the fibbonacci sequence for the number passed into it
+// HINT! fibbonacchi =
+//  c = a + b;
+//  a = b;
+//  b = c;
+function Fibbonacci(number) {}
+Fibbonacci();
+
+//11.--------------------------------------------------------------------------------------
+//BONUS!Implement fizzbuzz, if a number is divisible by 3: Fizz, if a number is divisible by 5: Buzz, if the number is divisible by both 3 and 5, FizzBuzz
+// https://www.geeksforgeeks.org/fizz-buzz-implementation/
+function fizzbuzz(number) {}
+
+fizzbuzz();
+
+//12.--------------------------------------------------------------------------------------
+//SUPERBONUS - we have not talked about recursion yet, but here is a fun one to try.
+//recursion is when a function calls itself.
+//Complete the function so that it returns the factorial of a number. Factorial (n!) is the product of all whole numbers between 1 and n
+//example: factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+function factorial(n) {
+  if (n === 0) return 1;
+  return; /// din kode her
+}
+console.log(factorial(5));
